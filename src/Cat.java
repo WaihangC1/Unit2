bcf11f9997f319c8411b6be343ddf1d48bb368ce
BT_1N3_34 <waihangchen@gmail.com>
2023-11-10 11:00:20 -0500

    added cat

--- src/Cat.java
+++ src/Cat.java
@@ -15,11 +15,16 @@ public class Cat {
 
     // method that introduces the Cat
     public void introduce() {
-        System.out.println("Hello my name is " + name);
+
+        if (age < 7) {
+            System.out.println("Hello my name is " + name + "and I am a younger cat.");
+        } else {
+            System.out.println("Hello my name is " + name + "and I am a older cat.");
+        }
     }
 
     // method that prints Cat info
-    public void printCatInfo() {
+         public void printCatInfo() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Weight: " + weight);
--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,7 +1,12 @@
 public class CatRunner {
     public static void main(String[] args) {
-        Cat cat1 = new Cat("Sam",5,10);
+        Cat cat1 = new Cat("Sam",8,10);
         cat1.printCatInfo();
+        cat1.introduce();
+
+        Cat cat2= new Cat("Nia",3,8);
+        cat2.introduce();
+        cat2.printCatInfo();
     }
 
 }
